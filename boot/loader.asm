6f71a00893efd785b7fa5f3450ffb533be6451ae
Amanda <aze@hawaii.edu>
2024-07-29 16:33:38 -1000

    Checking for long mode support

--- boot/loader.asm
+++ boot/loader.asm
@@ -4,19 +4,37 @@
 
 [ORG 0x7e00]    ;Directive: Loader runs at address 0x7e00
 
-;start: The start of the loader. Printing message Loader start.
+;start: The start of the loader. Checking for long mode support
 start:  
-    mov ah,0x13         ;
+    mov [DriveID], dl   ;Move [DriveID previously saved] dl to DriveID.
+
+    mov eax, 0x80000000 ;pass 0x80000001 to eax. 
+    cpuid               ;Returns processor identification and feature information.
+    cmp eax, 0x80000001 ;If the written value in eax < 0x80000001 then input value is not supported.
+    jb NotSupport       ;Jump if below
+
+    mov eax, 0x80000001 ;Input value: Processor Features.
+    cpuid
+    test edx, (1<<29)   ;Checking if bit 29  of edx is set.
+    jz NotSupport       ;Long mode not supported [zero flag set].
+    test edx, (1<<26)   ;Checking if bit 26 of edx is set.
+    jz NotSupport       ;1 GB page support Not Supported [zero flag set].
+
+    mov ah,0x13         
     mov al,1
     mov bx,0xa
     xor dx,dx
     mov bp,Message
     mov cx,MessageLen 
     int 0x10
-    
+
+
+NotSupport:
 End:
     hlt
     jmp End
 
-Message:    db "loader starts"
+
+DriveID:    db 0        ;Create DriveID variable
+Message:    db "long mode is supported"
 MessageLen: equ $-Message
