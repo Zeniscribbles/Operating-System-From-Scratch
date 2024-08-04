3420c4c2c4d66b8e0607b4f48fe1aed12bd9f957
Amanda <aze@hawaii.edu>
2024-08-03 16:16:46 -1000

    Retrieving the memory map.

--- boot/loader.asm
+++ boot/loader.asm
@@ -20,7 +20,7 @@ start:
     test edx, (1<<26)   ;Checking if bit 26 of edx is set.
     jz NotSupport       ;1 GB page support Not Supported [zero flag set].
 
-
+;Set up the Disk Address Packet (DAP) for extended read
 LoadKernal:
     mov si, ReadPacket      ;Load the address of ReadPacket into SI.
     mov word[si], 0x10      ;Set the size of the packet structure to 16 bytes.
@@ -29,11 +29,32 @@ LoadKernal:
     mov word[si+6], 0x1000  ;Set the segment part of the memory address to 0x1000.
     mov dword[si+8], 6      ;Set the starting LBA (low 32 bits) to 6.
     mov dword[si+0xc],0     ;Set the starting LBA (high 32 bits) to 0.
-    mov dl, DriveID         ;Load the drive ID from memory into DL.
+    mov dl, [DriveID]         ;Load the drive ID from memory into DL.
     mov ah, 0x42            ;Set AH to 0x42 (extended read function).
     int 0x13                ;Call BIOS interrupt 0x13 to read the sectors.
     jc ReadError            ;If there is an error (carry flag set), jump to ReadError.
 
+GetMemoryInfoStart:    
+    mov eax, 0xe820       ;Pass 0xe820 to eax.
+    mov edx, 0x534d4150   ;ASCHII code for the s-map to edx.
+    mov ecx, 20           ;Save length of the memory block [20] to edx.
+    mov edi, 0x9000       ;Save memory block address in edi.
+    xor ebx, ebx          ;Zero ebx before calling function.
+    int 0x15              ;Call BIOS interrupy 0x15 ().
+    jc  NotSupport
+
+GetMemoryInfo:
+    add edi, 20             ;Set edi to point to the next memory address
+    mov eax, 0xe820       ;Pass 0xe820 to eax.
+    mov edx, 0x534d4150   ;ASCHII code for the s-map to edx.
+    mov ecx, 20           ;Save length of the memory block [20] to edx.
+    int 0x15
+    jc GetMemoryDone
+
+    test ebx, ebx
+    jnz GetMemoryInfo
+
+GetMemoryDone:
     mov ah,0x13             ;Set AH to 0x13 (write string function).      
     mov al,1                ;Set AL to 1 (write mode).
     mov bx,0xa              ;Set BX to page number 0xa.
@@ -49,10 +70,10 @@ End:
     jmp End     ;Infinite loop to prevent execution past the end.
 
 
-DriveID:    db 0                    ;Variable to store the drive ID.
-Message:    db "Kernal is loaded"   ;Success message string.
-MessageLen: equ $-Message           ;Length of the success message string.
-ReadPacket: times 16 db 0           ;Define 16 bytes, each initialized to 0.
+DriveID:    db 0                        ;Variable to store the drive ID.
+Message:    db "Get Memory Info done"   ;Success message string.
+MessageLen: equ $-Message               ;Length of the success message string.
+ReadPacket: times 16 db 0               ;Define 16 bytes, each initialized to 0.
 
 ;Data structure for the extended read function. 
 ;Disk Address Packet (DAP) Structure:
