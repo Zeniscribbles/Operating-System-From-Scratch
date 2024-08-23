66c006ffa80d52d093d24a30e9ddb069cd35ab76
Amanda <aze@hawaii.edu>
2024-08-22 20:34:05 -1000

    Reloading the GDT, building the kernel

--- boot/kernel.asm
+++ boot/kernel.asm
@@ -7,6 +7,14 @@
 
 
 start:
+    lgdt [Gdt64Ptr]
+
+    push 8
+    push KernelEntry
+    db 0x48
+    retf
+
+KernelEntry:
     mov byte[0xb8000],'K'
     mov byte[0xb8001],0xa
 
@@ -15,3 +23,12 @@ End:
     hlt         ;Halt the CPU.
     jmp End     ;Infinite loop to prevent execution past the end.
 
+
+Gdt64:  
+    dq 0
+    dq 0x0020980000000000
+
+Gdt64Len: equ $-Gdt64
+
+Gdt64Ptr: dw Gdt64Len-1
+          dq Gdt64
